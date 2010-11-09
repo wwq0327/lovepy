78e1170753c76898a7c88002444504c13f78a8b9
wwq0327 <wwq0327@gmail.com>
2010-11-09 22:44:22 +0800

    dip

---
+++ dip/odbchelper.py
@@ -0,0 +1,10 @@
+def buildConnectionString(params):
+    return ";".join(["%s=%s" % (k, v) for k, v in params.items()])
+
+if __name__ == '__main__':
+    myParams = {'server':'mpilgrim',
+                'database':'master',
+                'uid':'sa',
+                'pwd':'secret'
+                }
+    print buildConnectionString(myParams)
